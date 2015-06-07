73e77698beb6058054ac59bec8d15e84e994b0e8
AKuo <auszon3@gmail.com>
2015-06-07 14:57:06 +0800

    Fix conflicts with previous declaration with the same Objective-C selector

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,16 +36,16 @@ class ViewController: UIViewController
             enter()
         }
         switch operation {
-            case "×": performOperation { $0 * $1 }
-            case "÷": performOperation { $1 / $0 }
-            case "+": performOperation { $0 + $1 }
-            case "−": performOperation { $1 - $0 }
-            case "√": performOperation { sqrt($0) }
+            case "×": performBinaryOperation { $0 * $1 }
+            case "÷": performBinaryOperation { $1 / $0 }
+            case "+": performBinaryOperation { $0 + $1 }
+            case "−": performBinaryOperation { $1 - $0 }
+            case "√": performUnaryOperation { sqrt($0) }
             default: break
         }
     }
     
-    func performOperation(operation: (Double, Double) -> Double)
+    func performBinaryOperation(operation: (Double, Double) -> Double)
     {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
@@ -53,7 +53,7 @@ class ViewController: UIViewController
         }
     }
 
-    func performOperation(operation: Double -> Double)
+    func performUnaryOperation(operation: Double -> Double)
     {
         if operandStack.count >= 1 {
             displayValue = operation(operandStack.removeLast())
